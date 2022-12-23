a4c875b9bb1f8549d988e7602cc94f6851f6bc46
eyup2000 <gulerr.eyup47@gmail.com>
2022-12-23 12:34:41 +0300

    synchronization

---
+++ src/test/java/com/arcane/tests/Day07_C20_synchronization2.java
@@ -0,0 +1,24 @@
+package com.arcane.tests;
+
+import com.arcane.utilities.TestBase;
+import org.junit.Test;
+
+public class Day07_C20_synchronization2 extends TestBase {
+
+    /*
+    Method olustur: isEnabled
+    https://the-internet.herokuapp.com/dynamic_controls adresine git
+    enable Button'una tikla
+    Mesajin “It's enabled!” oldugunu verify et
+    Textbox'in enabled oldugunu verify et (textbox'ta yazi yazilabilir)
+    Disable button'una tikla
+    Mesajin “It's disabled!” oldugunu verify et
+    Textbox'in disabled oldugunu verify et (textbox'ta yazi yazilamaz)
+
+    NOTE: .isEnabled(); bir elementin enabled olup olmadigini check eder
+     */
+@Test
+    public void isEnabled(){
+
+}
+}
